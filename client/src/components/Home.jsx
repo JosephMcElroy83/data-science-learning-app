43e42eaf198075dcb6d9366768417e6acc4626c4
Joseph McElroy <66266130+JosephMcElroy83@users.noreply.github.com>
2021-09-26 01:48:00 -0400

    started working on media queries to fix up the non-autofixing flex containers and text

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,7 +5,7 @@ import "./home.css"
 export default function Home() {
   return (
     <div className="home-div">
-      <img src="https://cdn.pixabay.com/photo/2018/09/27/09/22/artificial-intelligence-3706562_1280.jpg" alt="data science" className="home-image" />
+      <img className="home-image" src="https://cdn.pixabay.com/photo/2018/09/27/09/22/artificial-intelligence-3706562_1280.jpg" alt="data science" />
         
       <h1 className="home-h1">
         What is Data Science
@@ -14,7 +14,6 @@ export default function Home() {
       <p>
         Most people believe that data science is a field of study that just reviews numbers and calculations. This may be true, however the focus on data science is hypothesis testing and writing. What problems can you see and what can be done about it? This is a life long question through the scientific processes. With this in mind, if you saw a set of data and were asked to write a hypothesis on the spot, what would draw your eye first? How would you approach data itself; do visuals help you, maybe reading through some data charts to make connections in a side by side comparison? The strength that Data Science brings to the table is the ability to approach from almost any angle and see how various questions can bring results, what might yours bring?
       </p>
-
       <hr />
 
       <h1 className="home-h1">What Does Data Science Solve</h1>
@@ -22,7 +21,6 @@ export default function Home() {
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, iure provident libero sunt eveniet saepe earum maxime impedit. Iusto totam, mollitia magnam adipisci repellendus a? Fugit dolorum sit aperiam at molestias officiis earum nam corrupti. Suscipit voluptatem unde in reprehenderit molestiae aliquid animi aperiam, quibusdam facere deserunt laudantium expedita! Commodi optio molestiae nam magni, temporibus itaque consequuntur nulla vero ipsa obcaecati eum asperiores dolorum adipisci quam repellendus! Nesciunt mollitia, dolorem provident tempore ipsam qui cum quod architecto sint voluptate, ducimus eos. Saepe, ipsa deserunt? Neque quia nisi vel doloribus iure, ipsum, reiciendis cumque repudiandae doloremque quasi soluta rem quisquam necessitatibus.</p>
 
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Atque numquam sapiente tenetur unde, consequuntur ipsum quam error vero, commodi rerum quibusdam? Eveniet in earum perspiciatis dicta saepe temporibus animi odio quidem officiis? Fugiat, maxime. Repellat officia reiciendis molestiae facere beatae voluptatem quo sapiente hic nostrum nesciunt corporis veniam impedit temporibus, quidem blanditiis maxime! At, magnam? Aliquam facilis dolor velit. Omnis nihil, eligendi aliquid magnam tenetur laudantium accusamus quam quae deserunt odit eveniet suscipit quis itaque minus numquam quasi debitis totam eos. Tenetur quod vero dolorum optio in. Ex ducimus dolorem iste odit modi vitae perferendis sapiente repellat. Vel, excepturi doloremque.</p>
-      
       <hr />
       
       <h2 className="home-h2">How to Get Started</h2>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,10 +10,10 @@ export default function Navbar() {
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 3v2m6-2v2M9 19v2m6-2v2M5 9H3m2 6H3m18-6h-2m2 6h-2M7 19h10a2 2 0 002-2V7a2 2 0 00-2-2H7a2 2 0 00-2 2v10a2 2 0 002 2zM9 9h6v6H9V9z" />
         </svg></span></h1></Link>
 
-        <Link className="a-tag" to="/data"><h3 className="data-link">Data</h3></Link>
-        <Link className="a-tag" to="/questions"><h3 className="question-link">Analysis Questions</h3></Link>
-        <Link className="a-tag form-link" to="/form"><h3 >Form</h3></Link>
-        <Link className="a-tag" to="/responses"><h3 className="responses-link">Responses</h3></Link>
+        <Link className="a-tag" to="/data"><h3 className="data-link-h3">Data</h3></Link>
+        <Link className="a-tag" to="/questions"><h3 className="question-link-h3">Analysis Questions</h3></Link>
+        <Link className="a-tag form-link" to="/form"><h3 className="form-link-h3">Form</h3></Link>
+        <Link className="a-tag" to="/responses"><h3 className="responses-link-h3">Responses</h3></Link>
 
     </div>
   )
